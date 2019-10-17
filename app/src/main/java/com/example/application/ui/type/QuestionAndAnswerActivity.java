5395e299261b9b8d452148ee9f13be18bfa9db34
nepuwhliu <nepuwhliu@sina.com>
2019-10-17 16:18:48 +0800

    完善答题系统

---
+++ app/src/main/java/com/example/application/ui/type/QuestionAndAnswerActivity.java
@@ -0,0 +1,16 @@
+package com.example.application.ui.type;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.application.R;
+
+public class QuestionAndAnswerActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionandanswer);
+    }
+}
